e9f4a4f4911b2a90cef36b7f9f7caab56d08faa3
Liu Shuiyuan <1210257083@qq.com>
2020-08-01 21:40:55 +0800

    second

--- ToyReact.js
+++ ToyReact.js
@@ -3,38 +3,99 @@ class ElementWrapper {
         this.root = document.createElement(type)
     }
     setAttribute(name,value) {
+        if (name === 'className') {
+            name = 'class'
+        }
+        if (name.match(/^on([\s\S]+)$/)) {
+            const eventName = RegExp.$1.replace(/^[\s\S]/, s => s.toLocaleLowerCase())
+            this.root.addEventListener(eventName,value)
+        }
         this.root.setAttribute(name, value)
     }
     appendChild(vchild) {
-        vchild.mountTo(this.root)
+        const range = document.createRange()
+        if(this.root.children.length) {
+          range.setStartAfter(this.root.lastChild)
+          range.setEndAfter(this.root.lastChild)
+        } else {
+          range.setStart(this.root,0)
+          range.setEnd(this.root,0)   
+        }
+        vchild.mountTo(range)    
     }
-    mountTo(parent) {
-        parent.appendChild(this.root)
+    mountTo(range) {
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
 export class Component {
     constructor() {
         this.children = []
+        this.props = Object.create(null)
     }
     setAttribute(name,value) {
+        this.props[name] = value
         this[name] = value
     }
-    mountTo(parent) {
-       let vdom = this.render()
-       console.log(vdom)
-       vdom.mountTo(parent)
+    mountTo(range) {
+        this.range = range
+        this.update(range)
+    }
+    update() {
+        let placeholder = document.createComment('placeholder')
+        let range = document.createRange()
+        range.setStart(this.range.endContainer, this.range.endOffset)
+        range.setStart(this.range.endContainer, this.range.endOffset)
+        range.insertNode(placeholder)
+
+        this.range.deleteContents()
+        let vdom = this.render()
+        vdom.mountTo(this.range)        
     }
     appendChild(vchild) {
-        this.children.push(vchild)
+        const range = document.createRange()
+        if(this.root.children.length) {
+          range.setStartAfter(this.root.lastChild)
+          range.setEndAfter(this.root.lastChild)
+        } else {
+          range.setStart(this.root,0)
+          range.setEnd(this.root,0)   
+        }
+        vchild.mountTo(range)
+    }
+    setState(state) {
+        let merge = (oldState, newState) => {
+            for (const p in newState) {
+                if (newState.hasOwnProperty(p)) {
+                    const element = newState[p];
+                    if(typeof element === 'object') {
+                        if (oldState[p] !== 'object' ) {
+                            oldState[p] = {}
+                        }
+                        merge(oldState[p], newState[p])
+                    } else {
+                        oldState[p] = element
+                    }
+                }
+            }
+        }
+
+        if (!this.state && state) {
+            this.state = {}
+        }
+        merge(this.state, state)
+        this.update()
+        console.log(this.state)
     }
 }
 class TextWrapper {
     constructor(content){
         this.root = document.createTextNode(content)
     }
-    mountTo(parent) {
-        parent.appendChild(this.root)
+    mountTo(range) {
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
@@ -73,9 +134,14 @@ export const ToyReact = {
         return element
     }, 
     render(vdom, element) {
-       vdom.mountTo(element)
-       console.log(vdom)
-       console.log(element)
-    //    element.appendChild(vdom)
+      const range = document.createRange()
+      if(element.children.length) {
+        range.setStartAfter(element.lastChild)
+        range.setEndAfter(element.lastChild)
+      } else {
+        range.setStart(element,0)
+        range.setEnd(element,0)   
+      }
+      vdom.mountTo(range)
     }
 };
--- main.js
+++ main.js
@@ -1,19 +1,52 @@
 import { ToyReact, Component } from './ToyReact.js'
 
 
-class Mycomponent extends Component {
-    render() {
+class Board extends Component {
+    renderSquare(i) {
+      return <Square value={i} />;
+    }
+render() {
+    const status = 'Next player: X';
 
-        return <div>
-            <span>Hello</span> <span>World</span>
-            <div>{this.children}</div>
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
         </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+class Square extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: null
+      }
+    }
+    render() {
+      return (
+        <button className="square" onClick={() => this.setState({value: 'X'})}>
+          {this.state.value || ''}
+        </button>
+      );
     }
 }
-
-let a = <Mycomponent name="a" id="ida">
-       <div>111</div>
-     </Mycomponent>
+let a = <Board name="a" id="ida">
+     </Board>
 console.log(a)
 
 ToyReact.render(a, 
